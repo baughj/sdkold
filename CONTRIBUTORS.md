194e716bd5f70a8f8e99625f8e7e3019d56a45ad
Justin Baugh <baughj@hybrasyl.com>
2016-02-12 21:10:38 -0500

    Adding copyright information / headers / contributors info

---
+++ CONTRIBUTORS.md
@@ -0,0 +1,9 @@
+Hybrasyl SDK Contributors
+=========================
+
+The following people have contributed to the SDK project:
+
+Michael Norris <norrismiv@gmail.com>
+Justin Baugh <baughj@hybrasyl.com>
+Pete Lukich <pete.lukich@gmail.com>
+
--- HybrasylIntegration/HybrasylImaging/Properties/AssemblyInfo.cs
+++ HybrasylIntegration/HybrasylImaging/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("HybrasylImaging")]
-[assembly: AssemblyCopyright("Copyright ©  2015")]
+[assembly: AssemblyCopyright("Copyright © Hybrasyl Project 2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
